f489b614e519d210a6ed58c447da549ae9e0f272
krunal <krunaltule53@gmail.com>
2021-04-22 15:55:10 +0530

    exception handling with added features like disabling buttons

---
+++ script.js
@@ -0,0 +1,261 @@
+const numElements=document.getElementById("numElements");
+const speed=document.getElementById("numElements");
+
+const algorithm=document.getElementById("algorithm");
+
+const elementsContainer=document.getElementById('elements');
+
+const sortBtn=document.getElementById("sort");
+
+
+
+
+
+
+
+let numberOfElements=numElements.value;
+let numbers=[];
+
+
+
+
+
+
+
+
+
+let elementsDom;
+
+mountDom();
+
+function updateDom(){
+  elementsContainer.innerHTML="";
+   
+  numbers.forEach((number,id)=>{
+
+    let elementNode=document.createElement("div");
+    elementNode.classList.add("element");
+    elementNode.classList.add("normal");
+    elementNode.style.height=`${number}px`;
+    elementNode.innerText=`${number}`;
+   
+   
+    elementsContainer.appendChild(elementNode);
+    // console.log(elementNode);
+
+
+
+  })
+
+}
+
+
+
+
+function mountDom(){
+  
+  // console.log(numberOfElements);
+
+
+  for(let i=0;i<numberOfElements;i++){
+    numbers[i]=(Math.floor((Math.random() * 400) + 1));
+  }
+
+  elementsContainer.innerHTML="";
+   
+  numbers.forEach((number,id)=>{
+
+    let elementNode=document.createElement("div");
+    elementNode.classList.add("element");
+    elementNode.classList.add("normal");
+    elementNode.style.height=`${number}px`;
+    elementNode.innerText=`${number}`;
+    elementsContainer.appendChild(elementNode);
+    // console.log(elementNode);
+
+
+
+  })
+
+  elementsDom=document.querySelectorAll('.element');
+  
+
+  // console.log(numbers)
+
+}
+
+
+// function bubbleSort(){
+//   const elementArr=document.querySelectorAll(".element");
+//   let i=0,j=0;
+//   const outerloop=setInterval(()=>{
+//     j=0;
+//     if(i===numberOfElements-1){
+//       clearInterval(outerloop);
+//     }
+
+
+//     const innerloop=setInterval(()=>{
+//       [elementArr[j].style.height,elementArr[j+1].style.height]=[`${numbers[j]}px`,`${numbers[j+1]}px`];
+//       // console.log(j);
+//       j=j+1;
+//       if(j===numElements-i-1){
+//         clearInterval(innerloop);
+//       }
+//     },400)
+
+//     i=i+1;
+    
+//   },200)
+  
+  
+
+// }
+
+
+function bubbleSort(){
+  
+  let auxillaryArray=[...numbers];
+  const animations=[];
+  
+  for(let i=0;i<numberOfElements-1;i++){
+    for(let j=0;j<numberOfElements-i-1;j++){
+      
+      if(auxillaryArray[j]>auxillaryArray[j+1]){
+        animations.push([j,j+1,true]);
+        let temp=auxillaryArray[j];
+        auxillaryArray[j]=auxillaryArray[j+1];
+        auxillaryArray[j+1]=temp;
+      }
+      else{
+        animations.push([j,j+1,false]);
+      }
+
+    }
+  }
+
+  return animations;
+  
+ 
+}
+
+sorted=()=>{
+  // elementsContainer.innerHTML="";
+  numbers.forEach((number,id)=>{
+
+    
+      let elementNode=document.createElement("div");
+    elementNode.classList.add("element");
+    elementNode.classList.add("red");
+    elementNode.style.height=`${number}px`;
+    elementNode.innerText=`${number}`;
+    elementsContainer.appendChild(elementNode);
+    
+   
+   
+  }) 
+
+}
+
+function makeDisable(){
+  sortBtn.disabled=true;
+  numElements.disabled=true;
+  algorithm.disabled=true;
+}
+
+function makeEnable(){
+  sortBtn.disabled=false;
+  numElements.disabled=false;
+  algorithm.disabled=false;
+}
+   
+
+
+
+
+
+
+
+
+
+// event listeners
+
+
+numElements.addEventListener("change",(e)=>{
+
+  for(let i=0;i<numberOfElements;i++){
+    numbers.pop();
+  }
+
+  numberOfElements=e.target.value;
+  
+  mountDom();
+
+
+})
+
+sortBtn.addEventListener("click",()=>{
+    makeDisable();
+    console.log("hello");
+    
+    const animations=bubbleSort();
+    // console.log(animations);
+    console.log(numbers);
+    var i=0,count=0;
+    try{
+
+
+    var loop=setInterval(()=>{
+      if(i===animations.length-1){
+        makeEnable();
+        sorted();
+        clearInterval(loop);
+        
+      }
+
+      setTimeout(()=>{
+                
+
+        // elementsDom[animations[i][0]].classList.add("yellow");
+        // elementsDom[animations[i][1]].classList.add("yellow");
+        // elementsDom[animations[i][1]].classList.remove("yellow");
+        // elementsDom[animations[i][0]].classList.remove("yellow");
+        if(animations[i][2]===true){
+          console.log(animations[i][0]," ",animations[i][1]);
+          let temp=numbers[animations[i][0]];
+          numbers[animations[i][0]]=numbers[animations[i][1]];
+          numbers[animations[i][1]]=temp;
+          // [numbers[animations[i][0]],numbers[animations[i][1]]]=[numbers[animations[i][1]],numbers[animations[i][0]]]
+          
+        }
+        else{
+          elementsDom[animations[i][0]].classList.add("green");
+          elementsDom[animations[i][1]].classList.add("green");
+
+        }
+        elementsDom[animations[i][0]].classList.add("green");
+        elementsDom[animations[i][1]].classList.add("green");
+
+        updateDom();
+        i++;
+        count++;
+              
+      
+
+
+      },50);
+
+
+      
+      
+
+    },50)
+  }
+  catch(err){
+    // clearInterval(loop);
+    makeEnable();
+    // console.log(err);
+  }
+  
+})
+
