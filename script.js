b86803ea2813b1c4b4ff358c518e041f9903b54c
krunal <krunaltule53@gmail.com>
2021-04-23 17:16:33 +0530

    added selection sort

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const numElements=document.getElementById("numElements");
 const speedEl=document.getElementById("speed");
 const algorithm=document.getElementById("algorithm");
 const elementsContainer=document.getElementById('elements');
-const sortBtn=document.getElementById("sort");
+const sortBtn=document.querySelectorAll(".sort");
 const randomize=document.getElementById("randomize");
+const bubbleBtn=document.getElementById("bubble");
+const selectionBtn=document.getElementById("selection");
+const insertionBtn=document.getElementById("insertion");
+const quickBtn=document.getElementById("quick");
+const mergeBtn=document.getElementById("merge");
+// let algo=algorithm.value;
 var speed=speedEl.value;
 let numberOfElements=numElements.value;
 let numbers=[];
@@ -64,6 +70,151 @@ function bubbleSort(){
   return animations;
 }
 
+
+function selectionSort(){
+  let auxillaryArray=[...numbers];
+  const animations=[];
+  for(let i=0;i<numberOfElements-1;i++){
+    let minidx=i;
+    for(let j=i+1;j<numberOfElements;j++){
+      animations.push([minidx,j,false]);
+      if(auxillaryArray[j]<auxillaryArray[minidx]){
+        
+        minidx=j;
+      }
+    }
+    let temp=auxillaryArray[minidx];
+    auxillaryArray[minidx]=auxillaryArray[i];
+    auxillaryArray[i]=temp;
+    animations.push([minidx,i,true]);
+    
+
+  }
+
+  return animations;
+}
+
+function bubbleSortWithAnimation(animations){
+  makeDisable();
+  
+  var i=0,count=0;
+  try{
+  var loop=setInterval(()=>{
+    if(i===animations.length){
+      makeEnable();
+      sorted();
+      clearInterval(loop);
+    }
+
+    setTimeout(()=>{
+      removeClass(elementsDom[animations[i][0]],"yellow");
+      removeClass(elementsDom[animations[i][1]],"yellow");
+
+      addClass(elementsDom[animations[i][0]],"red");
+      addClass(elementsDom[animations[i][1]],"red");
+
+      if(animations[i][2]===true){
+
+        // updating dom bar
+        setHeight(elementsDom[animations[i][0]],numbers[animations[i][1]]);
+        setHeight(elementsDom[animations[i][1]],numbers[animations[i][0]]);
+        
+        // updating dom text
+        setText(elementsDom[animations[i][0]],numbers[animations[i][1]]);
+        setText(elementsDom[animations[i][1]],numbers[animations[i][0]]);
+
+        // actual swapping of numbers in array
+        let temp=numbers[animations[i][0]];
+        numbers[animations[i][0]]=numbers[animations[i][1]];
+        numbers[animations[i][1]]=temp;
+
+      }
+      setTimeout(()=>{
+        removeClass(elementsDom[animations[i][0]],"red");
+        removeClass(elementsDom[animations[i][1]],"red");
+
+        addClass(elementsDom[animations[i][0]],"yellow");
+        addClass(elementsDom[animations[i][1]],"yellow"); 
+      },speed);
+      i++;
+      count++;
+    },speed);
+  },speed)
+}
+catch(err){
+  makeEnable();
+}
+}
+
+
+function selectionSortWithAnimation(animations){
+
+  makeDisable();
+  
+  var i=0,count=0;
+  try{
+  var loop=setInterval(()=>{
+    if(i===animations.length){
+      makeEnable();
+      sorted();
+      clearInterval(loop);
+    }
+
+    setTimeout(()=>{
+
+      removeClass(elementsDom[animations[i][0]],"red");
+      removeClass(elementsDom[animations[i][1]],"red");
+
+      removeClass(elementsDom[animations[i][0]],"yellow");
+      removeClass(elementsDom[animations[i][1]],"yellow");
+
+ 
+
+      if(animations[i][2]===true){
+        // addClass(elementsDom[animations[i][0]],"red");
+        addClass(elementsDom[animations[i][1]],"red"); 
+
+        // updating dom bar
+        setHeight(elementsDom[animations[i][0]],numbers[animations[i][1]]);
+        setHeight(elementsDom[animations[i][1]],numbers[animations[i][0]]);
+        
+        // updating dom text
+        setText(elementsDom[animations[i][0]],numbers[animations[i][1]]);
+        setText(elementsDom[animations[i][1]],numbers[animations[i][0]]);
+
+        // actual swapping of numbers in array
+        let temp=numbers[animations[i][0]];
+        numbers[animations[i][0]]=numbers[animations[i][1]];
+        numbers[animations[i][1]]=temp;
+
+        setTimeout(()=>{
+          removeClass(elementsDom[animations[i][0]],"red");
+          removeClass(elementsDom[animations[i][1]],"red");
+        },speed)
+
+      }
+
+      else{
+        setTimeout(()=>{
+          addClass(elementsDom[animations[i][0]],"yellow");
+          addClass(elementsDom[animations[i][1]],"yellow");
+
+        },speed)
+      }
+      setTimeout(()=>{
+
+      },speed);
+      i++;
+      count++;
+    },speed);
+  },speed)
+}
+catch(err){
+  makeEnable();
+}
+
+}
+
 sorted=()=>{
   elementsContainer.innerHTML="";
   numbers.forEach((number,id)=>{
@@ -78,19 +229,23 @@ sorted=()=>{
 }
 
 function makeDisable(){
-  sortBtn.disabled=true;
+  sortBtn.forEach((btn)=>
+    btn.disabled=true
+  )
   numElements.disabled=true;
   speedEl.disabled=true;
-  algorithm.disabled=true;
-  // randomize.style.display=none;
+  // algorithm.disabled=true;
+  randomize.disabled=true;
 }
 
 function makeEnable(){
-  sortBtn.disabled=false;
+  sortBtn.forEach((btn)=>
+  btn.disabled=false
+)
   numElements.disabled=false;
   speedEl.disabled=false;
-  algorithm.disabled=false;
-  // randomize.style.display=block;
+  // algorithm.disabled=false;
+  randomize.disabled=false;
 }
    
 
@@ -126,6 +281,23 @@ function setText(domElement,height){
 
 // event listeners
 
+// algorithm.addEventListener("change",(e)=>{
+//   algo=e.target.value;
+// })
+
+
+bubbleBtn.addEventListener("click",()=>{
+  animations=bubbleSort();
+  console.log("bubble");
+  bubbleSortWithAnimation(animations);
+})
+
+
+selectionBtn.addEventListener("click",()=>{
+  animations=selectionSort();
+  console.log("selection");
+  selectionSortWithAnimation(animations);
+})
 
 randomize.addEventListener("click",()=>{
   mountDom();
@@ -143,56 +315,26 @@ numElements.addEventListener("change",(e)=>{
   mountDom();
 })
 
-sortBtn.addEventListener("click",()=>{
-    makeDisable();
-    const animations=bubbleSort();
-    var i=0,count=0;
-    try{
-    var loop=setInterval(()=>{
-      if(i===animations.length){
-        makeEnable();
-        sorted();
-        clearInterval(loop);
-      }
+// sortBtn.addEventListener("click",()=>{
 
-      setTimeout(()=>{
-        removeClass(elementsDom[animations[i][0]],"yellow");
-        removeClass(elementsDom[animations[i][1]],"yellow");
+//     console.log(algo)
+//     if(algo===1){
+//       animations=bubbleSort();
+//       console.log("bubble");
+//       bubbleSortWithAnimation(animations);
+//     }
 
-        addClass(elementsDom[animations[i][0]],"red");
-        addClass(elementsDom[animations[i][1]],"red");
+        
 
-        if(animations[i][2]===true){
+//       if(algo===2){
+//         animations=selectionSort();
+//         console.log("selection");
+//         selectionSortWithAnimation(animations);
+//       }
 
-          // updating dom bar
-          setHeight(elementsDom[animations[i][0]],numbers[animations[i][1]]);
-          setHeight(elementsDom[animations[i][1]],numbers[animations[i][0]]);
-          
-          // updating dom text
-          setText(elementsDom[animations[i][0]],numbers[animations[i][1]]);
-          setText(elementsDom[animations[i][1]],numbers[animations[i][0]]);
+        
 
-          // actual swapping of numbers in array
-          let temp=numbers[animations[i][0]];
-          numbers[animations[i][0]]=numbers[animations[i][1]];
-          numbers[animations[i][1]]=temp;
- 
-        }
-        setTimeout(()=>{
-          removeClass(elementsDom[animations[i][0]],"red");
-          removeClass(elementsDom[animations[i][1]],"red");
 
-          addClass(elementsDom[animations[i][0]],"yellow");
-          addClass(elementsDom[animations[i][1]],"yellow"); 
-        },speed);
-        i++;
-        count++;
-      },speed);
-    },speed)
-  }
-  catch(err){
-    makeEnable();
-  }
-  
-})
+    
+// })
 
